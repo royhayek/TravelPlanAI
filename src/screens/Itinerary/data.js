fe89e0eadb77c7d0ec5582ab67cbbb9d26719432
Roy <royhayek27@gmail.com>
2023-09-23 13:47:38 +0300

    Added coordinates to the prompt

--- src/screens/Itinerary/data.js
+++ src/screens/Itinerary/data.js
@@ -2,183 +2,169 @@ export const DUMMY_ITINERARY = {
   days: [
     {
       name: 'Day 1',
-      date: '2023-09-23',
+      date: '2023-09-26',
       activities: [
         {
-          name: 'Eiffel Tower Visit',
-          description: 'Visit to the iconic Eiffel Tower and its observation deck',
-          address: '5 Avenue Anatole France, 75007 Paris',
+          name: 'Blue Mosque',
+          description: "Visit the iconic Blue Mosque, one of Istanbul's most recognizable landmarks.",
+          address: 'Sultan Ahmet Mahallesi, 34122 Fatih/İstanbul, Turkey',
+          coordinates: {
+            lat: '41.00527',
+            long: '28.97696',
+          },
         },
         {
-          name: 'Arc de Triomphe Sightseeing',
-          description: 'Sightseeing of the famous Arc de Triomphe monument',
-          address: 'Place Charles de Gaulle, 75008 Paris',
+          name: 'Grand Bazaar',
+          description: 'Explore the Grand Bazaar, one of the largest and oldest covered markets in the world.',
+          address: 'Ulus Mahallesi, 34126 Fatih/İstanbul, Turkey',
+          coordinates: {
+            lat: '41.01073',
+            long: '28.95991',
+          },
         },
         {
-          name: 'Seine River Cruise',
-          description: 'Cruise along the Seine River to enjoy the view of the city',
-          address: 'Port de Suffren, 75007 Paris',
+          name: 'Hagia Sophia Museum',
+          description: 'Visit the Hagia Sophia Museum and admire its stunning architecture.',
+          address: 'Ayasofya Meydanı, Sultanahmet, 34122 Fatih/İstanbul, Turkey',
+          coordinates: {
+            lat: '41.00839',
+            long: '28.97824',
+          },
         },
         {
-          name: 'Louvre Museum Tour',
-          description: 'Tour of the world-famous Louvre Museum and its renowned works of art',
-          address: 'Rue de Rivoli, 75001 Paris',
+          name: 'Spice Bazaar',
+          description: 'Discover the vibrant Spice Bazaar, a historic market offering a variety of spices, fruits, and souvenirs.',
+          address: 'Mısır Çarşısı, Eminönü, 34116 Fatih/İstanbul, Turkey',
+          coordinates: {
+            lat: '41.012506',
+            long: '28.967242',
+          },
         },
       ],
     },
     {
       name: 'Day 2',
-      date: '2023-09-24',
+      date: '2023-09-27',
       activities: [
         {
-          name: 'Picnic at the Champs de Mars',
-          description: 'Picnic at the Champs de Mars park with views of the Eiffel Tower in the background',
-          address: 'Avenue Anatole France, 75007 Paris',
+          name: 'Bosphorus Boat Tour',
+          description: 'Take a relaxing boat tour along the Bosphorus River to take in the stunning views of Istanbul.',
+          address: 'Kabataş, 34349 Beşiktaş/İstanbul, Turkey',
+          coordinates: {
+            lat: '41.04244',
+            long: '29.00597',
+          },
         },
         {
-          name: 'Notre Dame Cathedral Tour',
-          description: 'Take a tour of the awe-inspiring Notre Dame Cathedral and its stunning architecture',
-          address: '6 Parvis Notre-Dame - Place Jean-Paul II, 75004 Paris',
+          name: 'Galata Tower',
+          description: 'Climb to the top of Galata Tower for the best views of the city.',
+          address: 'Galata Kulesi Sokak, Beyoğlu, 34421 İstanbul, Turkey',
+          coordinates: {
+            lat: '41.0227',
+            long: '28.9768',
+          },
         },
         {
-          name: 'Montmartre Village Exploration',
-          description: 'Explore the winding streets and art galleries of the famous Montmartre village',
-          address: 'Place du Tertre, 75018 Paris',
+          name: 'Taksim Square',
+          description: "Explore Taksim Square, the bustling heart of Istanbul's nightlife and modern culture.",
+          address: 'Taksim Meydanı, Beyoğlu, 34435 İstanbul, Turkey',
+          coordinates: {
+            lat: '41.038172',
+            long: '28.984487',
+          },
         },
         {
-          name: 'Sacre-Coeur Basilica Visit',
-          description: 'Visit the iconic Sacre-Coeur Basilica and its stunning views of Paris',
-          address: '35 Rue du Chevalier de la Barre, 75018 Paris',
+          name: 'Topkapı Palace',
+          description: "Visit the Topkapı Palace, the former residence of Ottoman sultans and one of Istanbul's most important historical sites.",
+          address: 'Topkapı Sarayı, 34122 Fatih/İstanbul, Turkey',
+          coordinates: {
+            lat: '41.013517',
+            long: '28.984207',
+          },
         },
       ],
     },
     {
       name: 'Day 3',
-      date: '2023-09-25',
+      date: '2023-09-28',
       activities: [
         {
-          name: 'Versailles Palace Tour',
-          description: 'Tour of the magnificent Palace of Versailles and its breathtaking gardens',
-          address: "Place d'Armes, 78000 Versailles",
+          name: 'Dolmabahçe Palace',
+          description: 'Check out Dolmabahçe Palace, a stunning 19th century Ottoman palace.',
+          address: 'Dolmabahçe Cd., Beşiktaş, 34357 İstanbul, Turkey',
+          coordinates: {
+            lat: '41.042069',
+            long: '29.006955',
+          },
         },
         {
-          name: 'Hike in the Forest of Fontainebleau',
-          description: 'Hike in the lush and beautiful Forest of Fontainebleau for a peaceful day in nature',
-          address: '95510 Fontainebleau',
+          name: 'Turkish Food Tour',
+          description: 'Take a Turkish Food Tour around the city to sample the best local dishes.',
+          address: 'Yerebatan Caddesi, 34122 Fatih/İstanbul, Turkey',
+          coordinates: {
+            lat: '41.008564',
+            long: '28.976787',
+          },
         },
         {
-          name: 'Chateau de Vaux-le-Vicomte Visit',
-          description: 'Visit the majestic Chateau de Vaux-le-Vicomte and its extensive grounds',
-          address: '77950 Maincy',
+          name: 'Istanbul Archaeology Museums',
+          description: 'Explore the Istanbul Archaeology Museums, home to a large collection of ancient artifacts.',
+          address: 'Osman Hamdi Bey Yokuşu, 34116 Fatih/İstanbul, Turkey',
+          coordinates: {
+            lat: '41.013732',
+            long: '28.977358',
+          },
         },
         {
-          name: "Dinner at La Tour d'Argent",
-          description: "Dinner at the iconic and world-famous La Tour d'Argent restaurant",
-          address: '15 Quai de la Tournelle, 75005 Paris',
+          name: 'Rüstem Pasha Mosque',
+          description: 'Visit the Rüstem Pasha Mosque, an ornate mosque renowned for its exquisite Iznik tiles.',
+          address: 'Rüstem Paşa Mahallesi, 34126 Fatih/İstanbul, Turkey',
+          coordinates: {
+            lat: '41.010681',
+            long: '28.959975',
+          },
         },
       ],
     },
     {
       name: 'Day 4',
-      date: '2023-09-26',
-      activities: [
-        {
-          name: 'Montparnasse Tower Visit',
-          description: 'Visit the iconic Montparnasse Tower and its observation deck for stunning views of the city',
-          address: '33 Avenue du Maine, 75015 Paris',
-        },
-        {
-          name: 'Aubrac Market Exploration',
-          description: 'Explore the Aubrac Market for Parisian souvenirs and unique finds',
-          address: 'Rue de la Tour-des-Dames, 75009 Paris',
-        },
-        {
-          name: 'Café de Flore Visit',
-          description: 'Visit the iconic Café de Flore for coffee and people watching',
-          address: '172 Boulevard Saint-Germain, 75006 Paris',
-        },
-        {
-          name: 'Shopping at Galeries Lafayette',
-          description: 'Shopping at the world-famous Galeries Lafayette department store',
-          address: '40 Boulevard Haussmann, 75009 Paris',
-        },
-      ],
-    },
-    {
-      name: 'Day 5',
-      date: '2023-09-27',
-      activities: [
-        {
-          name: 'Paris Catacombs Exploration',
-          description: 'Explore the spooky and mysterious catacombs of Paris',
-          address: '1 Avenue du Colonel Henri Rol-Tanguy, 75014 Paris',
-        },
-        {
-          name: 'Latin Quarter Walking Tour',
-          description: 'Take a walking tour of the charming Latin Quarter and its cobbled streets and bookstores',
-          address: 'Rue de la Huchette, 75005 Paris',
-        },
-        {
-          name: 'Dinner at Le Grand Vefour',
-          description: 'Dinner at the iconic and world-renowned Le Grand Vefour restaurant',
-          address: '17 Rue de Beaujolais, 75001 Paris',
-        },
-        {
-          name: 'Nightlife at La Bellevilloise',
-          description: 'Nightlife at the famous La Bellevilloise music venue and bar',
-          address: '19-21 Rue Boyer, 75020 Paris',
-        },
-      ],
-    },
-    {
-      name: 'Day 6',
-      date: '2023-09-28',
-      activities: [
-        {
-          name: 'Centre Pompidou Visit',
-          description: 'Visit the iconic Centre Pompidou and its modern art collection',
-          address: 'Place Georges-Pompidou, 75004 Paris',
-        },
-        {
-          name: 'Jardin des Plantes Exploration',
-          description: 'Explore the stunning Jardin des Plantes and its botanical gardens',
-          address: '57 Rue Cuvier, 75005 Paris',
-        },
-        {
-          name: 'Musée Rodin Visit',
-          description: 'Visit the iconic Musée Rodin and its world-renowned sculptures',
-          address: '79 Rue de Varenne, 75007 Paris',
-        },
-        {
-          name: "Dinner at Chez L'Ami Jean",
-          description: "Dinner at the famed Chez L'Ami Jean for Basque-style cuisine",
-          address: '27 Rue Malar, 75007 Paris',
-        },
-      ],
-    },
-    {
-      name: 'Day 7',
       date: '2023-09-29',
       activities: [
         {
-          name: 'Les Invalides Visit',
-          description: 'Visit Les Invalides and its military museum',
-          address: '129 Rue de Grenelle, 75007 Paris',
+          name: 'Basilica Cistern',
+          description: 'Visit the Basilica Cistern, an underground water storage system built during the Byzantine era.',
+          address: 'Alemdar Mh., 34122 Fatih/İstanbul, Turkey',
+          coordinates: {
+            lat: '41.008919',
+            long: '28.977705',
+          },
         },
         {
-          name: 'Pont Alexandre III Sightseeing',
-          description: 'Sightseeing of the iconic Pont Alexandre III bridge and its stunning architecture',
-          address: "Quai d'Orsay, 75007 Paris",
+          name: 'Istanbul Modern',
+          description: 'Explore Istanbul Modern, a contemporary art museum located in a converted warehouse.',
+          address: 'Meclis-i Mebusan Caddesi, Antrepo 4, 34433 Beyoğlu/İstanbul, Turkey',
+          coordinates: {
+            lat: '41.036067',
+            long: '29.002119',
+          },
         },
         {
-          name: 'Luxembourg Gardens Exploration',
-          description: 'Explore the stunning Luxembourg Gardens with its greenery and ponds',
-          address: 'Rue de Vaugirard, 75006 Paris',
+          name: 'Kadıköy Market',
+          description: 'Visit the Kadıköy Market to explore the bustling stalls and sample local dishes.',
+          address: 'Kadıköy, İstanbul, Turkey',
+          coordinates: {
+            lat: '40.983783',
+            long: '29.063537',
+          },
         },
         {
-          name: 'Ladurée Visit',
-          description: 'Visit the iconic Ladurée for macarons and pastries',
-          address: '75 Av des Champs-Élysées, 75008 Paris',
+          name: 'Bebek Park',
+          description: 'Relax in the tranquil Bebek Park, a picturesque park located on the European side of Istanbul.',
+          address: 'Bebek Caddesi, Bebek, 34342 Beşiktaş/İstanbul, Turkey',
+          coordinates: {
+            lat: '41.045618',
+            long: '29.023064',
+          },
         },
       ],
     },
